26c2d5856fdf38c58537c9dcef2dda1cc1a6ee3a
Alexey Malov <alexey.malov@ispringsolutions.com>
2018-03-17 11:46:13 +0300

    Initial commit

---
+++ samples/SampleMVPApp/SampleMVPApp.h
@@ -0,0 +1,35 @@
+﻿
+//  : main header file for the SampleMVPApp application
+//
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'stdafx.h' before including this file for PCH"
+#endif
+
+#include "resource.h"       // main symbols
+
+
+// SampleApp:
+// See  for the implementation of this class
+//
+
+class SampleApp : public CWinApp
+{
+public:
+	SampleApp();
+
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+	virtual int ExitInstance();
+
+// Implementation
+
+public:
+	afx_msg void OnAppAbout();
+	DECLARE_MESSAGE_MAP()
+};
+
+extern SampleApp theApp;
